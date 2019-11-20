9e085144079a45a032330a6670c59f8f3a76fe40
Lê Thanh Vũ <thanhvule0310@gmail.com>
2019-11-20 08:17:27 +0700

    Updates

---
+++ precache-manifest.dc7b4cd6df55889c77a20fb1b87a5b38.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "426a77ca824c2cc366cd398bae752f44",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "ec4821ee8b6df07538d3",
+    "url": "/Todo/static/js/2.26881eea.chunk.js"
+  },
+  {
+    "revision": "8172ea141d4803543bb0",
+    "url": "/Todo/static/js/main.4e0d6f37.chunk.js"
+  },
+  {
+    "revision": "a289bed64cbf6c325dec",
+    "url": "/Todo/static/js/runtime-main.28b21b0c.js"
+  }
+]);
